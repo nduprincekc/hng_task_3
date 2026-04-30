648ac26ce1f289014baa28efc6dec69933e47bc0
kc emma <nduagubaemma@gmail.com>
2026-04-30 14:17:21 +0100

    Modify CORS settings for middleware
    Updated CORS middleware to allow all origins and disabled credentials.

--- app/main.py
+++ app/main.py
@@ -26,12 +26,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://insighta-web-chi.vercel.app",
-        "http://localhost:3000",
-        "http://localhost:3001",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
